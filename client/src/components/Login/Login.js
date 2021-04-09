779c2ddf46e03d481159bff7b2f3fa669417263f
swadhinroutray <swadhin.routray@gmail.com>
2021-04-10 00:40:43 +0530

    Added notifying toaster and fixed multiple auth functions

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,80 +1,101 @@
-import React, { Component } from 'react';
-import { Pane, TextInputField, Button, Text, Link, toaster, Heading } from 'evergreen-ui';
-import './Login.scss';
-import Axios from 'axios';
+import React, { Component } from "react";
+import {
+  Pane,
+  TextInputField,
+  Button,
+  Text,
+  Link,
+  toaster,
+  Heading,
+} from "evergreen-ui";
+import "./Login.scss";
+import Axios from "axios";
 
 // Axios config
 Axios.defaults.baseURL = process.env.REACT_APP_SERVER;
 
 export default class Login extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			email: '',
-			password: ''
-		};
-	}
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
 
-	async componentDidMount() {
-		if (new URLSearchParams(this.props.location.search).get('status') === 'false') {
-			toaster.warning('Please login first.');
-		}
-	}
+  async componentDidMount() {
+    if (
+      new URLSearchParams(this.props.location.search).get("status") === "false"
+    ) {
+      toaster.warning("Please login first.");
+    }
+  }
 
-	handleSubmit = async (e) => {
-		try {
-			e.preventDefault();
-			const { email, password } = this.state;
-			let user = await Axios.post('/login', { email: email, password: password });
-			console.log(user);
-			if (user.status === 200) {
-				console.log('Login success.');
-				localStorage.setItem('isSignedIn', true);
-				localStorage.setItem('nameState', user.data.name);
-				window.location.href = '/';
-			}
-		} catch (err) {
-			console.log(err);
-		}
-	};
+  handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const { email, password } = this.state;
+      let user = await Axios.post("/login", {
+        email: email,
+        password: password,
+      });
+      console.log(user);
+      if (user.status === 200) {
+        console.log("Login success.");
+        localStorage.setItem("isSignedIn", true);
+        localStorage.setItem("nameState", user.data.name);
+        window.location.href = "/";
+      } else {
+        toaster.warning("Wrong Credentials");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	render() {
-		return (
-			<div className='login-container'>
-				<Pane className='login-pane' elevation={1}>
-					<form onSubmit={this.handleSubmit}>
-						<Heading size={700} margin='default'>
-							Login
-						</Heading>
-						<TextInputField
-							required
-							name='email'
-							label='Email'
-							inputHeight={30}
-							inputWidth={250}
-							onChange={(e) => this.setState({ email: e.target.value })}
-							value={this.state.email}
-						/>
-						<TextInputField
-							required
-							type='password'
-							name='password'
-							label='Password'
-							inputHeight={30}
-							inputWidth={250}
-							onChange={(e) => this.setState({ password: e.target.value })}
-							value={this.state.password}
-						/>
-						<Button type='submit' appearance='primary'>
-							Login
-						</Button>
-						<Text marginTop={24} marginBottom={8} display='block' textAlign='center'>
-							Forgot your password? <Link color='blue'>Reset your password</Link>
-						</Text>
-					</form>
-				</Pane>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="login-container">
+        <Pane className="login-pane" elevation={1}>
+          <form onSubmit={this.handleSubmit}>
+            <Heading size={700} margin="default">
+              Login
+            </Heading>
+            <TextInputField
+              required
+              name="email"
+              label="Email"
+              inputHeight={30}
+              inputWidth={250}
+              onChange={(e) => this.setState({ email: e.target.value })}
+              value={this.state.email}
+            />
+            <TextInputField
+              required
+              type="password"
+              name="password"
+              label="Password"
+              inputHeight={30}
+              inputWidth={250}
+              onChange={(e) => this.setState({ password: e.target.value })}
+              value={this.state.password}
+            />
+            <Button type="submit" appearance="primary">
+              Login
+            </Button>
+            <Text
+              marginTop={24}
+              marginBottom={8}
+              display="block"
+              textAlign="center"
+            >
+              Forgot your password?{" "}
+              <Link color="blue">Reset your password</Link>
+            </Text>
+          </form>
+        </Pane>
+      </div>
+    );
+  }
 }
--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import Axios from 'axios';
-import { Pane, TextInputField, Button, Heading, toaster } from 'evergreen-ui';
-import ReCAPTCHA from 'react-google-recaptcha';
-import './Register.scss';
+import React, { Component } from "react";
+import Axios from "axios";
+import { Pane, TextInputField, Button, Heading, toaster } from "evergreen-ui";
+import ReCAPTCHA from "react-google-recaptcha";
+import "./Register.scss";
 
 // Axios config
 Axios.defaults.baseURL = process.env.REACT_APP_SERVER;
@@ -10,128 +10,153 @@ Axios.defaults.baseURL = process.env.REACT_APP_SERVER;
 const recaptchaRef = React.createRef();
 
 export default class Register extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			firstName: '',
-			lastName: '',
-			email: '',
-			password: '',
-			passwordConfirmed: '',
-			phone: ''
-		};
-	}
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      passwordConfirmed: "",
+      phone: "",
+    };
+  }
 
-	handleSubmit = async (e) => {
-		try {
-			e.preventDefault();
-			let { firstName, lastName, email, password, passwordConfirmed, phone } = this.state;
-			firstName = firstName.trim();
-			lastName = lastName.trim();
-			email = email.trim();
-			password = password.trim();
-			passwordConfirmed = passwordConfirmed.trim();
-			phone = phone.trim();
-			const token = await recaptchaRef.current.executeAsync();
-			if (password !== passwordConfirmed) {
-				// Passwords don't match!
-				console.log("Passwords don't match.");
-				toaster.warning("Passwords don't match!");
-			} else {
-				let resp = await Axios.post('/register', {
-					name: firstName + ' ' + lastName,
-					email: email,
-					password: password,
-					phone: phone,
-					token: token
-				});
-				console.log(resp);
-			}
-		} catch (err) {
-			console.log(err);
-		}
-	};
+  handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      let {
+        firstName,
+        lastName,
+        email,
+        password,
+        passwordConfirmed,
+        phone,
+      } = this.state;
+      firstName = firstName.trim();
+      lastName = lastName.trim();
+      email = email.trim();
+      password = password.trim();
+      passwordConfirmed = passwordConfirmed.trim();
+      phone = phone.trim();
+      const token = await recaptchaRef.current.executeAsync();
+      if (password !== passwordConfirmed) {
+        // Passwords don't match!
+        console.log("Passwords don't match.");
+        toaster.warning("Passwords don't match!");
+      }
+      if (password.length < 8) {
+        toaster.warning("Minimum Length Of Passowrd should be 8 characters");
+      } else {
+        let resp = await Axios.post("/register", {
+          name: firstName + " " + lastName,
+          email: email,
+          password: password,
+          phone: phone,
+          token: token,
+        });
+        console.log(resp);
+        await setTimeout(() => {
+          toaster.notify("Registered Successfully");
+        }, 2000);
+        if (resp.status == 200) {
+          window.location.href = "/login?status=false";
+        } else {
+          console.log("error Registering");
+          toaster.warning("Something went wrong, please try again later!");
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	render() {
-		return (
-			<div className='register-container'>
-				<Pane className='register-pane' elevation={1}>
-					<form onSubmit={this.handleSubmit}>
-						<Heading size={700} margin='default'>
-							Register
-						</Heading>
-						<TextInputField
-							required
-							name='first_name'
-							label='First Name'
-							inputHeight={30}
-							inputWidth={250}
-							onChange={(e) => this.setState({ firstName: e.target.value })}
-							value={this.state.firstName}
-						/>
+  render() {
+    return (
+      <div className="register-container">
+        <Pane className="register-pane" elevation={1}>
+          <form onSubmit={this.handleSubmit}>
+            <Heading size={700} margin="default">
+              Register
+            </Heading>
+            <TextInputField
+              required
+              name="first_name"
+              label="First Name"
+              inputHeight={30}
+              inputWidth={250}
+              onChange={(e) => this.setState({ firstName: e.target.value })}
+              value={this.state.firstName}
+            />
 
-						<TextInputField
-							required
-							name='last_name'
-							label='Last Name'
-							inputHeight={30}
-							inputWidth={250}
-							onChange={(e) => this.setState({ lastName: e.target.value })}
-							value={this.state.lastName}
-						/>
+            <TextInputField
+              required
+              name="last_name"
+              label="Last Name"
+              inputHeight={30}
+              inputWidth={250}
+              onChange={(e) => this.setState({ lastName: e.target.value })}
+              value={this.state.lastName}
+            />
 
-						<TextInputField
-							required
-							name='email'
-							label='Email'
-							inputHeight={30}
-							inputWidth={250}
-							onChange={(e) => this.setState({ email: e.target.value })}
-							value={this.state.email}
-						/>
+            <TextInputField
+              required
+              name="email"
+              label="Email"
+              inputHeight={30}
+              inputWidth={250}
+              onChange={(e) => this.setState({ email: e.target.value })}
+              value={this.state.email}
+            />
 
-						<TextInputField
-							required
-							type='password'
-							name='password'
-							label='Password'
-							inputHeight={30}
-							inputWidth={250}
-							onChange={(e) => this.setState({ password: e.target.value })}
-							value={this.state.password}
-						/>
+            <TextInputField
+              required
+              type="password"
+              name="password"
+              label="Password"
+              inputHeight={30}
+              inputWidth={250}
+              onChange={(e) => this.setState({ password: e.target.value })}
+              value={this.state.password}
+            />
 
-						<TextInputField
-							required
-							type='password'
-							name='password_confirmation'
-							label='Confirm Password'
-							inputHeight={30}
-							inputWidth={250}
-							onChange={(e) => this.setState({ passwordConfirmed: e.target.value })}
-							value={this.state.passwordConfirmed}
-						/>
+            <TextInputField
+              required
+              type="password"
+              name="password_confirmation"
+              label="Confirm Password"
+              inputHeight={30}
+              inputWidth={250}
+              onChange={(e) =>
+                this.setState({ passwordConfirmed: e.target.value })
+              }
+              value={this.state.passwordConfirmed}
+            />
 
-						<TextInputField
-							required
-							type='tel'
-							name='phone'
-							label='Mobile Number'
-							inputHeight={30}
-							inputWidth={250}
-							onChange={(e) => this.setState({ phone: e.target.value })}
-							value={this.state.phone}
-						/>
+            <TextInputField
+              required
+              type="tel"
+              name="phone"
+              label="Mobile Number"
+              inputHeight={30}
+              inputWidth={250}
+              onChange={(e) => this.setState({ phone: e.target.value })}
+              value={this.state.phone}
+            />
 
-						<ReCAPTCHA ref={recaptchaRef} size='invisible' sitekey='6LfnPuEZAAAAANbADW2JK_7hw2i3cY8Op0sKvudN' />
+            <ReCAPTCHA
+              ref={recaptchaRef}
+              size="invisible"
+              sitekey="6LfnPuEZAAAAANbADW2JK_7hw2i3cY8Op0sKvudN"
+            />
 
-						<Button type='submit' appearance='primary'>
-							Sign Up
-						</Button>
-					</form>
-				</Pane>
-			</div>
-		);
-	}
+            <Button type="submit" appearance="primary">
+              Sign Up
+            </Button>
+          </form>
+        </Pane>
+      </div>
+    );
+  }
 }
